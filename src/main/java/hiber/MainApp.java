82e819e2ba3c3daa081bd4fd7dc6bbfc1d2a52fa
blankkode <preabvuthy9@gmail.com>
2024-07-03 00:03:35 +0900

    task 3.1

---
+++ src/main/java/hiber/MainApp.java
@@ -0,0 +1,37 @@
+package hiber;
+
+import hiber.config.AppConfig;
+import hiber.model.Car;
+import hiber.model.User;
+import hiber.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class MainApp {
+   public static void main(String[] args) throws SQLException {
+      AnnotationConfigApplicationContext context = 
+            new AnnotationConfigApplicationContext(AppConfig.class);
+
+      UserService userService = context.getBean(UserService.class);
+
+      userService.add(new User("User1", "Lastname1", "user1@mail.ru", new Car("Toyota", 2020)));
+      userService.add(new User("User2", "Lastname2", "user2@mail.ru", new Car("Honda", 2021)));
+      userService.add(new User("User3", "Lastname3", "user3@mail.ru", new Car("Ford", 2022)));
+      userService.add(new User("User4", "Lastname4", "user4@mail.ru", new Car("BMW", 2023)));
+
+      List<User> users = userService.listUsers();
+      for (User user : users) {
+         System.out.println("Id = "+user.getId());
+         System.out.println("First Name = "+user.getFirstName());
+         System.out.println("Last Name = "+user.getLastName());
+         System.out.println("Email = "+user.getEmail());
+         System.out.println("Car Model = " + user.getCar().getModel());
+         System.out.println("Car Series = " + user.getCar().getSeries());
+         System.out.println();
+      }
+
+      context.close();
+   }
+}
---
+++ src/main/java/hiber/model/Car.java
@@ -0,0 +1,48 @@
+package hiber.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String model;
+    private int series;
+
+    public Car() {}
+
+    public Car(String model, int series) {
+        this.model = model;
+        this.series = series;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+}
